2941bf8dd88016dc2c93051dd53b0a39b8ae8c55
Shubham Pateria <shubham.pateria@gmail.com>
2024-05-20 12:29:12 +0800

    Update data_model_configs.py

--- configs/data_model_configs.py
+++ configs/data_model_configs.py
@@ -18,18 +18,6 @@ class EEG():
           src_ids = [did]
           trgt_ids = [other_did for other_did in self.domain_ids if other_did not in src_ids]
           self.scenarios.append( (src_ids,trgt_ids) ) 
-        '''self.scenarios = [
-                  ([2], [13, 0, 10, 5, 11]), 
-                  ([18], [13, 3, 9, 10, 6]), 
-                  ([0], [17, 16, 3, 11, 7]), 
-                  ([12], [15, 9, 14, 6, 18]), 
-                  ([19], [9, 5, 18, 11, 2]), 
-                  ([18], [19, 7, 16, 6, 8]), 
-                  ([11], [16, 8, 14, 15, 18]), 
-                  ([8], [16, 5, 7, 10, 14]), 
-                  ([5], [3, 0, 10, 1, 18]), 
-                  ([9], [2, 11, 17, 12, 6])
-        ]'''
         
         # data parameters
         self.num_classes = 5
@@ -69,14 +57,6 @@ class FD():
           src_ids = [did]
           trgt_ids = [other_did for other_did in self.domain_ids if other_did not in src_ids]
           self.scenarios.append( (src_ids,trgt_ids) ) 
-        
-        #self.scenarios = [ ([1],[0,3]) ]
-        '''self.scenarios = [
-                ([0], [1, 2, 3]), 
-                ([1], [0, 2, 3]), 
-                ([2], [0, 1, 3]), 
-                ([3], [0, 1, 2])
-              ]'''
               
         self.sequence_len = 5120         
         self.class_names = ['Healthy', 'D1', 'D2']
@@ -122,20 +102,6 @@ class HAR():
           src_ids = [did]
           trgt_ids = [other_did for other_did in self.domain_ids if other_did not in src_ids]
           self.scenarios.append( (src_ids,trgt_ids) )
-         
-        #self.scenarios = [ ([1], [2,3,5]) ] 
-        '''self.scenarios = [
-                ([8], [22, 1, 29, 14, 24]), 
-                ([4], [9, 1, 5, 17, 23]), 
-                ([20], [4, 26, 1, 7, 14]), 
-                ([28], [6, 7, 24, 12, 5]), 
-                ([22], [29, 25, 5, 10, 14]), 
-                ([18], [15, 17, 20, 8, 4]), 
-                ([19], [20, 6, 22, 13, 21]), 
-                ([14], [5, 19, 6, 18, 25]), 
-                ([13], [24, 7, 22, 6, 17]), 
-                ([26], [11, 2, 10, 27, 14])
-              ]'''
               
         self.class_names = ['walk', 'upstairs', 'downstairs', 'sit', 'stand', 'lie']
         self.sequence_len = 128
